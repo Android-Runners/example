de97127e581d76d1d604cff025cdb03c65ab016f
Daniil Muntyan <daniilmuntjan@gmail.com>
2017-08-07 19:35:19 +0300

    My first pushing

--- src/example/Example.java
+++ src/example/Example.java
@@ -51,7 +51,7 @@ public class Example {
 
         @Override
         public void run() {
-            for(int i = 0; i < 24; ++i) {
+            for(int i = 0; i < 12; ++i) {
                 try {
                     getScreen();
                 } catch (AWTException | IOException e) {
